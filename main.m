b5e36326c0b7d9c0e1eff7c6e9e2ddc7838830bc
Min Liu <wzygzlm@gmail.com>
2018-03-20 17:37:54 +0100

    Try to use matlab live script.

--- main.m
+++ main.m
@@ -2,13 +2,17 @@
 clear, clc, close ALL
 
 [file,path,indx] = uigetfile('*.bag', 'Select a File', '/media/minliu/dataset/MVSEC'); %gets directory
+fullname = fullfile(path, file);
 if isequal(file,0)
    disp('User selected Cancel')
 else
-   disp(['User selected ', fullfile(path, file)])
+   disp(['User selected ', fullname])
 end
 
-bagselect = rosbag(fullfile(path, file));
+bagselect = rosbag(fullname);
+
+% Support in R2018a
+% rosbag info fullname
 
 pose_topicd = select(bagselect,'Time',...
-    [bagselect.StartTime bagselect.StartTime + 1],'Topic','/davis/left/pose');
+    [bagselect.StartTime bagselect.EndTime],'Topic','/davis/left/pose');
