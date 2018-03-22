ed3e76edb3baf671d19990adc5685f34ab96031a
Min Liu <wzygzlm@gmail.com>
2018-03-22 12:33:38 +0100

    Depth is fused.

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 %% Read data
 clear, clc, close ALL
+format long
 
 [file,path,indx] = uigetfile('*.bag', 'Select a File', '/media/minliu/dataset/MVSEC'); %gets directory
 fullname = fullfile(path, file);
@@ -28,7 +29,7 @@ fy = left_camera_K(5);
 cx = left_camera_K(3);
 cy = left_camera_K(6);
 
-left_depth = select(bag,'Time', [bag.StartTime bag.StartTime + 1],'Topic','/davis/left/depth_image_raw')
+left_depth = select(bag,'Time', [bag.StartTime bag.EndTime],'Topic','/davis/left/depth_image_raw')
 left_depth_msg = readMessages(left_depth,'DataFormat','struct');
 
 
@@ -40,6 +41,8 @@ left_pose_orientation = quat2rotm(reshape([Orientations.X Orientations.Y Orienta
 left_pose_translation = reshape([Positions.X Positions.Y Positions.Z], numel(Positions), 3);
 left_pose_ts = cellfun(@(m) RosTs2MatlabSec(m.Header.Stamp), left_pose_msg);
 left_pose_LieAg = Pose2LieAg(left_pose_orientation, left_pose_translation, left_pose_ts);
+
+left_depth_ts = cellfun(@(m) RosTs2MatlabSec(m.Header.Stamp), left_depth_msg);
 
 %% Plot poses
 % pose_plot(left_pose.NumMessages, Positions, Orientations);
@@ -69,31 +72,34 @@ h_legend=legend('X','Y','Z');
 start_time = bag.StartTime;
 time_delta = 1;
 
-vx = zeros(260,346);
-vy = zeros(260,346);
-PointCloud = ones(260, 346, 3);
+height = 260;
+width = 346;
+vx = zeros(height,width);
+vy = zeros(height,width);
+OF_GT = zeros(left_pose.NumMessages, 2, height, width);
+PointCloud = ones(height, width, 3);
 PointCloud(:,:,1) = cumsum(PointCloud(:,:,1)) - 1;
 PointCloud(:,:,2) = cumsum(PointCloud(:,:,2), 2) - 1;
-depth = ones(260,346);
+depth = ones(height,width);
 
  for i=2:left_pose.NumMessages   
             %% Data association
 %             event_ts = RosTs2MatlabSec(Events(j).Ts);
-%             index_association = find((left_pose_ts - event_ts) <= 0);
-%             % Make sure there're at least 2 poses, so one can be use the
-%             % new one and the other as the old one.
-%             if (numel(index_association) <= 2)
-%                 continue;
-%             end
-%             i = index_association(numel(index_association));
+            currentPoseTs = left_pose_ts(i);
+            index_association = find(left_depth_ts <= currentPoseTs);
+            % Make sure it is not empty
+            if (numel(index_association) <= 1)
+                continue;
+            end
+            depth_index = index_association(numel(index_association));
             
             %% OF conversion
 %              event_x = Events(j).X;
 %              event_y = Events(j).Y;
-                dataArray = left_depth_msg{1}.Data;
+                dataArray = left_depth_msg{depth_index}.Data;
                 dataArray = reshape(dataArray, 1, 4*left_depth_msg{1}.Height*left_depth_msg{1}.Width);
                 depthFloat = typecast( dataArray , 'single') ;
-                depth = reshape(depthFloat, 260, 346);
+                depth = reshape(depthFloat, height, width);
                 PointCloud(:,:,1) = (PointCloud(:,:,1).* depth - cx)/fx;
                 PointCloud(:,:,2) = (PointCloud(:,:,2).* depth -cy)/fx;
                 PointCloud(:,:,3) = depth;
@@ -106,10 +112,10 @@ depth = ones(260,346);
                 Z = reshape(PointCloud(:,:,3), 1, []);             
 
 
-                % Reshape the result to [2*260*346, 6] with every 2 rows are
+                % Reshape the result to [2*height*width, 6] with every 2 rows are
                 % belong to one image point.
-                temp = double([fx./Z, zeros(1, 260*346), -fx.*X./Z, -fx.*X.*Y./Z.^2, fx + fx.*X.^2./Z.^2, -fx.*Y./Z;...
-                                 zeros(1, 260*346), fy./Z, -fy.*Y./Z,  -fy - fx.*Y.^2./Z.^2, fy.*X.*Y./Z.^2, fy.*X./Z]);
+                temp = double([fx./Z, zeros(1, height*width), -fx.*X./Z, -fx.*X.*Y./Z.^2, fx + fx.*X.^2./Z.^2, -fx.*Y./Z;...
+                                 zeros(1, height*width), fy./Z, -fy.*Y./Z,  -fy - fx.*Y.^2./Z.^2, fy.*X.*Y./Z.^2, fy.*X./Z]);
                 temp = temp';
                 temp = reshape(temp, [], 12);
                 temp = temp';
@@ -120,14 +126,16 @@ depth = ones(260,346);
                 lie_alg = temp * left_pose_LieAg(i,:)';    
                 lie_alg = reshape(lie_alg, 2, []);
 
-                vx = reshape(lie_alg(1,:), 260, 346);
-                vy = reshape(lie_alg(2,:), 260, 346);
+                vx = reshape(lie_alg(1,:), height, width);
+                vy = reshape(lie_alg(2,:), height, width);
+                OF_GT(i, 1, :, :) = vx;
+                OF_GT(i, 2, :, :) = vy;
                 %              offset = double([fx/Z, 0, -fx*X/Z, -fx*X*Y/Z^2, fx + fx*X^2/Z^2, -fx*Y/Z;...
                 %                                      0, fy/Z, -fy*Y/Z,  -fy - fx*Y^2/Z^2, fy*X*Y/Z^2, fy*X/Z]) * left_pose_LieAg(i,:)';   
                 %              vx(event_x + 1, event_y + 1) = offset(1);
                 %              vy(event_x + 1, event_y + 1) = offset(2);
-                OF_GT = opticalFlow(vx, vy);
-                plot(OF_GT, 'DecimationFactor',[10 10],'ScaleFactor',10);
+                flow = opticalFlow(vx, vy);
+                plot(flow, 'DecimationFactor',[10 10],'ScaleFactor',10);
             if mod(i, itv) ~= 0
                 continue;
             end
@@ -180,5 +188,6 @@ depth = ones(260,346);
 %             view(az,el);
             drawnow;
 %             pause(delay);  % in second                 
-end
+ end
 
+save('OF_GT.mat', 'OF_GT'); 
